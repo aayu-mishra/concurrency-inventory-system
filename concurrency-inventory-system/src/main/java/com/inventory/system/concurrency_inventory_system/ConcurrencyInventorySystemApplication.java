cfc682ecebe3ad8af42b511fee7a2dad5cbfe9d3
Ayush Mishra <mishraayush010@gmail.com>
2025-08-16 00:15:52 +0530

    added Repository

---
+++ concurrency-inventory-system/src/main/java/com/inventory/system/concurrency_inventory_system/ConcurrencyInventorySystemApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.system.concurrency_inventory_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyInventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyInventorySystemApplication.class, args);
+	}
+
+}
---
+++ concurrency-inventory-system/src/test/java/com/inventory/system/concurrency_inventory_system/ConcurrencyInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory.system.concurrency_inventory_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyInventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
